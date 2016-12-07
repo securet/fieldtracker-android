d5d3811aec7d21d8a12f15b66eb1aff2fe686882
kmdon <manjunatha.kanekal@allsmart.in>
2016-12-07 12:41:59 +0530

    1. Camera screen is modified 2. History ListItem Listner is added

---
+++ app/src/main/java/com/oppo/sfamanagement/model/Promoter.java
@@ -0,0 +1,17 @@
+package com.oppo.sfamanagement.model;
+
+/**
+ * Created by allsmartlt218 on 02-12-2016.
+ */
+
+public class Promoter {
+    private String promoter;
+
+    public String getPromoter() {
+        return promoter;
+    }
+
+    public void setPromoter(String promoter) {
+        this.promoter = promoter;
+    }
+}
