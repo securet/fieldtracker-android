8802c5b45c461ee0ab755eaeab8a9d9166b58a81
kmdon <manjunatha.kanekal@allsmart.in>
2016-12-07 12:47:07 +0530

    1. Camera screen is modified 2. History ListItem Listner is added

---
+++ app/src/main/java/com/oppo/sfamanagement/model/Store.java
@@ -0,0 +1,17 @@
+package com.oppo.sfamanagement.model;
+
+/**
+ * Created by allsmartlt218 on 01-12-2016.
+ */
+
+public class Store {
+    private String store;
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+}
